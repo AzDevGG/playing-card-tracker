d57c026108ea14073d3b8cc40501fc5e382da272
AzDevGG <beasthunter97@gmail.com>
2025-02-12 18:03:55 +0700

    Update cards and suits order

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
-const suits = ["♠", "♣", "♥", "♦"];
-const ranks = ["A", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K"];
+const suits = ["♠", "♣", "♦", "♥"];
+const ranks = ["3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A", "2"];
 const cardGrid = document.getElementById("card-grid");
 const resetBtn = document.getElementById("reset-btn");
 
